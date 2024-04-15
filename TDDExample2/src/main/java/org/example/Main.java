73db48ed79f3d2641fb7ac33c7ed765feb4d528e
Mert Çelik <109079231+mertcelikdev@users.noreply.github.com>
2024-04-15 19:39:06 +0300

    Delete TDDExample2 directory

--- TDDExample2/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        float result = Calculator.divide(10, 0);
-        System.out.println(result);
-    }
-}
